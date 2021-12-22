06d0fb08fe9f06b282212f2d657689f3c3e98a82
Korotenko Alex <alexey.korotenko90@gmail.com>
2021-12-22 15:56:20 +0200

    first

---
+++ src/Components/Filter/Filter.js
@@ -0,0 +1,19 @@
+import React  from 'react'
+import { ImputEnter, InputType, InputText } from "../FormComponents";
+import PropTypes from 'prop-types';
+
+
+const Filter = ({onChange}) => {
+    return (   
+        <InputType onChange={onChange}>
+            <InputText>Find contact by name</InputText>
+            <ImputEnter />
+        </InputType>
+   )
+};
+
+Filter.prototype = {
+    onChange: PropTypes.func,
+}
+
+export default Filter;
